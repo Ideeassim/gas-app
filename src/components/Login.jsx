1ba06ee3f464d68790523acaa46d2a7d95593d5b
Ideeassim <137564264+Ideeassim@users.noreply.github.com>
2025-09-24 11:24:26 +0100

    api edits2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,7 +27,7 @@ const navigate= useNavigate()
 
 const handleLogin = async () => {
     try {
-      const res = await api.post("/login", {
+      const res = await api.post("/api/login", {
         username,
         password,
       });
